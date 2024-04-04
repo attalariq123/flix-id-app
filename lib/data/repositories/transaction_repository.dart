16c1c31442f295a076bba756a0941c26ab2bed92
attalariq123 <attalariq002@gmail.com>
2024-04-04 22:12:15 +0700

    update: finishing firebase auth and repo

--- lib/data/repositories/transaction_repository.dart
+++ lib/data/repositories/transaction_repository.dart
@@ -4,5 +4,5 @@ import 'package:flix_id/domain/entities/transaction.dart';
 abstract interface class TransactionRepository {
   Future<Result<Transaction>> createTransaction(
       {required Transaction transaction});
-  Future<Result<Transaction>> getUserTransaction({required String uid});
+  Future<Result<List<Transaction>>> getUserTransaction({required String uid});
 }
--- lib/presentation/providers/repositories/authentication/authentication_provider.dart
+++ lib/presentation/providers/repositories/authentication/authentication_provider.dart
@@ -1,8 +1,9 @@
-import 'package:flix_id/data/dummies/dummy_authentication.dart';
+import 'package:flix_id/data/firebase/firebase_authentication.dart';
 import 'package:flix_id/data/repositories/authentication.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'authentication_provider.g.dart';
 
 @riverpod
-Authentication authentication(AuthenticationRef ref) => DummyAuthentication();
+Authentication authentication(AuthenticationRef ref) =>
+    FirebaseAuthentication();
