756bc800875db572877ca7cc226caf404c1c5727
attalariq123 <attalariq002@gmail.com>
2024-04-20 13:23:44 +0700

    complete providers for repo

--- lib/presentation/providers/repositories/transaction_repository/transaction_repository_provider.g.dart
+++ lib/presentation/providers/repositories/transaction_repository/transaction_repository_provider.g.dart
@@ -1,25 +1,28 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'user_repository_provider.dart';
+part of 'transaction_repository_provider.dart';
 
 // **************************************************************************
 // RiverpodGenerator
 // **************************************************************************
 
-String _$userRepositoryHash() => r'1a3a9f80ec27bfb78bd4ac37319076992ed8dbf4';
+String _$transactionRepositoryHash() =>
+    r'e21e9bedc8f1f9af8794d244b17cd366a5ae0ee3';
 
-/// See also [userRepository].
-@ProviderFor(userRepository)
-final userRepositoryProvider = AutoDisposeProvider<UserRepository>.internal(
-  userRepository,
-  name: r'userRepositoryProvider',
+/// See also [transactionRepository].
+@ProviderFor(transactionRepository)
+final transactionRepositoryProvider =
+    AutoDisposeProvider<TransactionRepository>.internal(
+  transactionRepository,
+  name: r'transactionRepositoryProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
       ? null
-      : _$userRepositoryHash,
+      : _$transactionRepositoryHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-typedef UserRepositoryRef = AutoDisposeProviderRef<UserRepository>;
+typedef TransactionRepositoryRef
+    = AutoDisposeProviderRef<TransactionRepository>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
