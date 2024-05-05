347348b337054199bdd2fa60a5baa53e7f83bb04
attalariq123 <attalariq002@gmail.com>
2024-05-05 20:02:24 +0700

    feat: add router

--- lib/presentation/pages/login_page/login_page.dart
+++ lib/presentation/pages/login_page/login_page.dart
@@ -1,8 +1,5 @@
-// import 'package:flix_id/data/dummies/dummy_authentication.dart';
-// import 'package:flix_id/data/dummies/dummy_user_repository.dart';
-import 'package:flix_id/domain/usecases/login/login.dart';
-import 'package:flix_id/presentation/pages/main_page/main_page.dart';
-import 'package:flix_id/presentation/providers/usecase/login_provider.dart';
+import 'package:flix_id/presentation/providers/router/router_provider.dart';
+import 'package:flix_id/presentation/providers/user_data/user_data_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -11,29 +8,25 @@ class LoginPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen(userDataProvider, (previous, next) {
+      if (next is AsyncData) {
+        if (next.value != null) {
+          ref.read(routerProvider).goNamed('main');
+        } else if (next is AsyncError) {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(SnackBar(content: Text(next.error.toString())));
+        }
+      }
+    });
+
     return Scaffold(
       appBar: AppBar(title: const Text('Login')),
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            Login login = ref.watch(loginProvider);
-
-            login(LoginParams(email: 'atta@gmail.com', password: 'atta123'))
-                .then((result) => {
-                      if (result.isSuccess)
-                        {
-                          Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) =>
-                                MainPage(user: result.resultValue!),
-                          ))
-                        }
-                      else
-                        {
-                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                            content: Text(result.errorMessage!),
-                          ))
-                        }
-                    });
+            ref
+                .read(userDataProvider.notifier)
+                .login(email: 'atta@gmail.com', password: 'atta123');
           },
           child: const Text('Login'),
         ),
